be82f939d0a9fe17664aec99660dc29dd4b9f7f9
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-12-06 00:15:23 +0600

    LinkedList in action

--- src/main/java/kz/zhelezyaka/queue/WorkingWithQueues.java
+++ src/main/java/kz/zhelezyaka/queue/WorkingWithQueues.java
@@ -1,10 +1,29 @@
 package kz.zhelezyaka.queue;
 
 import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.Queue;
 
 public class WorkingWithQueues {
     public static void main(String[] args) {
+        LinkedList<Person> linkedList = new LinkedList<>();
+        linkedList.add(new Person("Svetlana", 34));
+        linkedList.add(new Person("Alexa", 23));
+        linkedList.addFirst(new Person("Lybov", 38));
+
+        final ListIterator<Person> personListIterator = linkedList.listIterator();
+        while (personListIterator.hasNext()) {
+            System.out.println(personListIterator.next());
+        }
+
+        System.out.println("-------------------------");
+
+        while (personListIterator.hasPrevious()) {
+            System.out.println(personListIterator.previous());
+        }
+    }
+
+    private static void queues() {
         Queue<Person> supermarket = new LinkedList<>();
         supermarket.add(new Person("Vladimir", 99));
         supermarket.add(new Person("Nikolay", 93));
